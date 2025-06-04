907d9d9d33727c1d876ef6f98e337bc10e2bc759
Stanislau Kviatkouski <7zete7@gmail.com>
2025-06-04 15:36:04 +0300

    feat: Integrate Mercure for real-time updates
    - Added MercureBundle and its configuration.
    - Updated `.env` with Mercure environment variables.
    - Modified `composer.json` and `composer.lock` to include Mercure dependencies.
    - Registered MercureBundle in `bundles.php`.

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
